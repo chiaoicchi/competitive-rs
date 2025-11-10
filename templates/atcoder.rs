76f679f4c98c3639d33a5ec040aa425aec042977
chiaoi <chiaoi@pm.me>
2025-11-10 18:30:44 +0900

    chore;settings

---
+++ templates/atcoder.rs
@@ -0,0 +1,11 @@
+#[allow(unused)]
+use itertools::Itertools;
+use proconio::input;
+#[allow(unused)]
+use proconio::marker::*;
+#[allow(unused)]
+use std::collections::*;
+
+fn main() {
+    input! {}
+}
